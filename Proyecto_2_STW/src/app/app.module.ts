fceb8bae70b4b978d6f4f3f33edb41a974bb6e54
ManuelR11 <rod21509@uvg.edu.gt>
2023-05-18 23:57:03 -0600

    Inicio de pagina

--- Proyecto_2_STW/src/app/app.module.ts
+++ Proyecto_2_STW/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameBOYComponent } from './game-boy/game-boy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBOYComponent
   ],
   imports: [
     BrowserModule
---
+++ Proyecto_2_STW/src/app/game-boy/game-boy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-boy',
+  templateUrl: './game-boy.component.html',
+  styleUrls: ['./game-boy.component.css']
+})
+export class GameBOYComponent {
+
+} 
